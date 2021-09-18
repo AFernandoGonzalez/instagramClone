405c34cd7346a10dba0f0c8dbcac5cec32ff6b5a
Fernando Gonzalez <58346389+AFernandoGonzalez@users.noreply.github.com>
2021-09-17 21:53:26 -0400

    implemented sign up feature

--- instagramClone/AppDelegate.swift
+++ instagramClone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // --- Copy this only
+            
+            let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "T6N9G2OovWInj77ipN6232zOxs4CN9nYcqF62JeQ" // <- UPDATE
+                    $0.clientKey = "BxZkJyqOAx3U6ix8sByI2rp6kgwJBXWtPiAtSmFv" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+            
+            // --- end copy
+        
+        
+        
         return true
     }
 
